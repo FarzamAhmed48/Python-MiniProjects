be8179a3cd0e63ec95f0e23fcb97ce92b50a3a81
FarzamAhmed48 <farzamahmed48@gmail.com>
2025-01-29 09:57:07 +0500

    Basic File Handling

---
+++ fileIO.py
@@ -0,0 +1,17 @@
+
+'''
+This will Open a file and read the content of the file
+'''
+# openFile=open("file.txt")
+# data=openFile.read()
+# print(data)
+# openFile.close()
+'''
+This will Open a file and write the content of the file
+'''
+# myStr="Mera naam jaiga ismein ab"
+# file=open("file.txt","w")
+# file.write(myStr)
+# file.close()
+
+# 
