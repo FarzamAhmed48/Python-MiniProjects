d7a0c27a01ec2e7fbd6b36854a40a396e7bbb18a
Farzam Ahmed <farzamahmed48@gmail.com>
2025-02-24 21:48:56 +0500

    Super Keyword Implemented

--- Inheritance.py
+++ Inheritance.py
@@ -6,10 +6,13 @@ class Employee:
 class Developer(Employee):
     Salary="200 crore"
     def __init__(self):
-        pass
+        print("Constructor of Developer Class")
 
 class last(Developer):
     name="Farzam Ahmed"
+    def __init__(self):
+        super().__init__()
+
     def myself(self):
         print(f"My name is {self.name} and the company where i do work is {self.company} paying me {self.Salary} for my serivces")
 
